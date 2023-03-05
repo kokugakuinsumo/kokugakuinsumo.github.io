2d6595ead92d6d8b6c4ee69610028815531bd642
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2023-03-04 17:17:41 -0800

    Update

--- src/main/cljs/kokugakuinsumo/views/mail.cljs
+++ src/main/cljs/kokugakuinsumo/views/mail.cljs
@@ -3,5 +3,5 @@
 (defn mail-page []
   [:div {:id "mail"}
    [:row
-    [:iframe {:src "https://ws.formzu.net/fgen/S19196376/"
+    [:iframe {:src "https://ws.formzu.net/fgen/S827135345/"
               :style {:height "600px" :max-width "600px" :width "100%" :border 0}}]]])
